1d8ae0f0f7b238a8ccb6a4d7e9e4794e4a5d59eb
Muhammad Jilani <120803991+jilani192@users.noreply.github.com>
2022-12-25 23:02:25 +0500

    Add files via upload

---
+++ sum2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num, sum = 0;
+    cout << "Enter number 1: ";
+    cin >> num;
+    sum = sum + num;
+    cout << "Enter number 2: ";
+    cin >> num;
+    sum = sum + num;
+    cout << "Enter number 3: ";
+    cin >> num;
+    sum = sum + num;
+    cout << "Enter number 4: ";
+    cin >> num;
+    sum = sum + num;
+    cout << "Enter number 5: ";
+    cin >> num;
+    sum = sum + num;
+    cout << "Sum is = " << sum << endl;
+    return 0;
+}
